46234bd4e5315a173edb98bf7a124073248ecabb
unknown <souhepend@hotmail.com>
2013-11-21 21:52:29 +0100

    added calendar layout and scrollbars

--- Web/static/js/app.js
+++ Web/static/js/app.js
@@ -1,3 +1,3 @@
 "use strict";
 
-var taskViewer = angular.module('taskViewer', []);
+var taskViewer = angular.module('taskViewer', ['perfect_scrollbar']);
--- Web/static/js/controllers/taskControllers.js
+++ Web/static/js/controllers/taskControllers.js
@@ -1,13 +1,11 @@
 'use strict';
-
 (function(){
     var taskStatus = {
         new: "active",
         removed: "removed"
     };
-    //TODO: controller
-    //var tasksControllers = angular.module('tasksControllers', []);
-    
+
+    //TASKS CONTROLLER
     taskViewer.controller('TasksController', ['$scope', 'storageService', function TasksController($scope, storageService){
         var tasks = $scope.tasks = storageService.get();
         $scope.selectedIndex = 0; 
@@ -43,5 +41,21 @@
             $scope.filters = filter;
             $scope.selectedIndex = $index;
         }        
+    }]);
+    
+    //CALENDAR CONTROLER
+    taskViewer.controller('CalendarController', ['$scope', function CalendarController($scope){      
+        $scope.hours = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];    
+        $scope.days = ["18 NOV 2013", "19 NOV 2013", "20 NOV 2013", "21 NOV 2013", "22 NOV 2013", "23 NOV 2013", "24 NOV 2013"];
+
+        //Actions
+        $scope.goToNextWeek = function(){
+          
+        };
+        
+        //Actions
+        $scope.goToPreviousWeek = function(){
+          
+        };
     }]);
 })();
