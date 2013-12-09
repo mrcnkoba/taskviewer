c61c7ca2eb6684e622342cf92899e6f23d6ac93f
souhe <souhepend@hotmail.com>
2013-12-09 22:30:06 +0100

    bugs fixed

--- Web/static/js/controllers/calendarController.js
+++ Web/static/js/controllers/calendarController.js
@@ -1,21 +1,22 @@
 'use strict';
-(function(){
-     //CALENDAR CONTROLER
-    taskViewer.controller('CalendarController', ['$scope', 'dateTimeService', function CalendarController($scope, dts){   
-        var actualMonday = dts.getMonday(new Date());
-        $scope.hours = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];    
-        $scope.days = dts.createWeek(new Date());
-        
-        
-        //Actions
-        $scope.goToNextWeek = function(){
-            actualMonday = new Date(actualMonday.getFullYear(),actualMonday.getMonth(), actualMonday.getDate()+7);
-            $scope.days = dts.createWeek(actualMonday);
-        };
-        
-        $scope.goToPreviousWeek = function(){
-            actualMonday = new Date(actualMonday.getFullYear(),actualMonday.getMonth(), actualMonday.getDate()-7);
-            $scope.days = dts.createWeek(actualMonday); 
-        };
+(function () {
+    //CALENDAR CONTROLER
+    taskViewer.controller('CalendarController', ['$scope', 'dateTimeService',
+        function CalendarController($scope, dts) {
+            var actualMonday = dts.getMonday(new Date());
+            $scope.hours = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
+            $scope.days = dts.createWeek(new Date());
+
+
+            //Actions
+            $scope.goToNextWeek = function () {
+                actualMonday = new Date(actualMonday.getFullYear(), actualMonday.getMonth(), actualMonday.getDate() + 7);
+                $scope.days = dts.createWeek(actualMonday);
+            };
+
+            $scope.goToPreviousWeek = function () {
+                actualMonday = new Date(actualMonday.getFullYear(), actualMonday.getMonth(), actualMonday.getDate() - 7);
+                $scope.days = dts.createWeek(actualMonday);
+            };
     }]);
 })();
--- Web/static/js/controllers/taskController.js
+++ Web/static/js/controllers/taskController.js
@@ -1,51 +1,55 @@
 'use strict';
-(function(){
+(function () {
     var taskStatus = {
         new: "active",
         removed: "removed"
     };
 
     //TASKS CONTROLLER
-    taskViewer.controller('TasksController', ['$scope', 'storageService', function TasksController($scope, storageService){
-        var tasks = $scope.tasks = storageService.get();
-        $scope.selectedIndex = 0; 
-        
-        $scope.$watch('tasks', function(newVal, oldVal){
-            if(newVal !== oldVal){
-                storageService.put(tasks);    
-            }
-        }, true);
-        
-        $scope.$watch('tasks.task.done', function(newVal, oldVal){
-            if(newVal !== true){
-                this.status = taskStatus.done;   
-            }
-        }, true);
-        
-        $scope.filters={};
-        
-        //Actions
-        $scope.add = function(){
-            $scope.tasks.push({
-                title: this.newTaskTitle,
-                description: "",
-                validToDate: new Date(),
-                done: false,
-                priority: 1,
-                status: taskStatus.new
-            })
-            this.newTaskTitle = '';
-        };
-        
-        $scope.toggleEdit = function(task){
-            task.editMode = !task.editMode;         
-        }
-        
-        $scope.applyFilter = function(filter, $index){
-            $scope.filters = filter;
-            $scope.selectedIndex = $index;
-        }        
-    }]);
-    
+    taskViewer.controller('TasksController', ['$scope', 'storageService',
+        function TasksController($scope, storageService) {
+            var tasks = $scope.tasks = storageService.get();
+            $scope.selectedIndex = 0;
 
+            $scope.$watch('tasks', function (newVal, oldVal) {
+                if (newVal !== oldVal) {
+                    storageService.put(tasks);
+                }
+            }, true);
+
+            $scope.$watch('tasks.task.done', function (newVal, oldVal) {
+                if (newVal !== true) {
+                    this.status = taskStatus.done;
+                }
+            }, true);
+
+            $scope.filters = {};
+
+            //Actions
+            $scope.add = function () {
+                $scope.tasks.push({
+                    title: this.newTaskTitle,
+                    description: "",
+                    validToDate: new Date(),
+                    done: false,
+                    priority: 1,
+                    status: taskStatus.new
+                })
+                this.newTaskTitle = '';
+            };
+
+            $scope.toggleEdit = function (task) {
+                task.editMode = !task.editMode;
+                zz
+            }
+
+            $scope.remove = function (task) {
+                //TODO: remove task         
+            }
+
+            $scope.applyFilter = function (filter, $index) {
+                $scope.filters = filter;
+                $scope.selectedIndex = $index;
+            }
+    }]);
 })();
