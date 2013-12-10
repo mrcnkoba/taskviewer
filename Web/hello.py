3369495ae500579c9ebca7000b6052bad7b655a1
souhe <souhepend@hotmail.com>
2013-12-10 20:55:09 +0100

    changed folder structure

--- Web/hello.py
+++ Web/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -6,7 +6,7 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
 
 @app.route('/hello/')
