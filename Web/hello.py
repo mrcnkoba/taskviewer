b2fbd52daa5e9b41f37e080c31616f0a435070e4
klouczers <klouczers@gmail.com>
2013-12-22 17:40:42 +0100

    added rest methods for flask

--- Web/hello.py
+++ Web/hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, abort
+from Service.TaskService import TaskService
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -9,10 +10,21 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/hello/')
-def hello():
-    return jsonify(id='first', title='hello world')
+@app.route('/api/task', methods=['POST'])
+def create():
+    if not request.json:
+        abort(400)
+    title = request.form['title']
+    task_service = TaskService()
+    task_service.create_task(title)
+    return jsonify(success=True)
 
 
+@app.route('/api/task', methods=['GET'])
+def get_tasks():
+    task_service = TaskService()
+    tasks = task_service.get_all_tasks()
+    return jsonify(tasks=tasks)
+    
 if __name__ == '__main__':
     app.run(debug=True)
